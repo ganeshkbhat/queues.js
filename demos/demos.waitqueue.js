5b36e486a09ff8538f3813727bb155bcd76aec2d
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-01-07 11:28:34 +0530

    [COMMIT] [Improvement] Adding architecture docs for Queues, Stack, and DEnqueue. Adding top and bottom queues. Adding waitqueue

---
+++ demos/demos.waitqueue.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Package: scale.concurrency
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i scale.concurrency --save
+ * Github: https://github.com/ganeshkbhat/scale.concurrency
+ * npmjs Link: https://www.npmjs.com/package/scale.concurrency
+ * File: .js
+ * File Description: .js 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+var Queue = require("../index.js").WaitQueue;
+var waitqueue = new Queue();
